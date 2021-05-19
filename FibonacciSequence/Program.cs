cf71988930faca826d2e1d531018bd8cc2020a1f
abitj <https://github.com/abitj>
2021-05-19 06:34:44 -0700

    FibonacciSquence solution

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// C# program that iteratively solves Fibonacci 
+
+namespace FibonacciSequence
+
+{
+    public static class FibonacciSequence
+    {
+        public static void Main(string[] args)
+        {//
+            //Defines variables
+            int firstNumber = 0, secondNumber = 1, thirdNumber, i, userNumber;
+            //User Input
+            Console.WriteLine();
+            Console.Write("Enter the number of elements: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            //Prints 0 and 1
+            Console.Write(firstNumber + " \n" + secondNumber + " \n");
+            //Starts loop from 2 
+            for (i = 2; i < userNumber; ++i)
+            {
+                thirdNumber = firstNumber + secondNumber;
+                Console.Write(thirdNumber + " \n");
+                firstNumber = secondNumber;
+                secondNumber = thirdNumber;
+            }
+            //readability
+            Console.Write("------------------------------------------------------------------\n");
+        }
+    }
+}
+
